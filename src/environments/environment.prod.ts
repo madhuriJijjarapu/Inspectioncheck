6a953d274df0169ec7b33c6908ca5a736c12e13a
madhuriJijjarapu <madhuri.jijjarapu@carbynetech.com>
2023-04-04 17:52:25 +0530

    apiurl changed i prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://inspectionplanning.azurewebsites.net/'
+  // apiUrl: 'https://inspectionplanning.azurewebsites.net/',
+  apiUrl: 'http://127.0.0.1:5000/'
 
 };
